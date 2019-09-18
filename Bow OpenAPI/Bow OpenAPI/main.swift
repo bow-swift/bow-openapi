2de0414ca1a5d4623682664e54c3798572db832a
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-09-18 11:56:56 +0200

    initialized project

---
+++ Bow OpenAPI/Bow OpenAPI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Bow OpenAPI
+//
+//  Created by Miguel Angel on 18/09/2019.
+//  Copyright © 2019 47 Degrees. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

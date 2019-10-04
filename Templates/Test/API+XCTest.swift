6517c8c8e59d9c364e317ef2aa2f9f0dd274a251
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-10-04 19:04:57 +0200

    working on api send xctests

--- Templates/Test/API+XCTest.swift
+++ Templates/Test/API+XCTest.swift
@@ -5,15 +5,119 @@ import Bow
 import BowEffects
 
 
-extension Either where A == API.HTTPError, B: Equatable {
-    func assert(success expected: B, _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+public extension XCTest {
+    func assert<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                              apiConfig: API.Config,
+                              succeeds: T,
+                              _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        envIO.provide(apiConfig)
+             .unsafeRunSyncEither()
+             .assert(success: succeeds)
+    }
+    
+    func assert<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                              apiConfig: API.Config,
+                              failures: Error,
+                              _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        envIO.provide(apiConfig)
+             .unsafeRunSyncEither()
+             .assert(error: .other(error: Mock.Error.general))
+    }
+    
+    func assertMalformedURL<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                          apiConfig: API.Config,
+                                          _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .malformedURL(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assertParsingError<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                          apiConfig: API.Config,
+                                          _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .parsingError(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assertBadRequest<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                        apiConfig: API.Config,
+                                        _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .badRequest(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assertForbidden<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                       apiConfig: API.Config,
+                                        _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .forbidden(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assertNotFound<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                      apiConfig: API.Config,
+                                      _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .notFound(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assertServerError<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                         apiConfig: API.Config,
+                                         _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .serverError(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assertServiceUnavailable<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                         apiConfig: API.Config,
+                                         _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .serviceUnavailable(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assertUnknown<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                         apiConfig: API.Config,
+                                         _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .unknown(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+    
+    func assert<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>,
+                                         apiConfig: API.Config,
+                                         _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+        
+        let (response, httpResponse, data) = perform(envIO, with: apiConfig)
+        response.assert(error: .unknown(response: httpResponse, data: data), information: information, file: file, line: line)
+    }
+}
+
+// MARK: - helpers
+
+typealias HTTPErrorData<T> = (response: Either<API.HTTPError, T>, httpResponse: URLResponse, data: Data)
+
+private extension XCTest {
+    func perform<T: Equatable>(_ envIO: EnvIO<API.Config, API.HTTPError, T>, with apiConfig: API.Config) -> HTTPErrorData<T> {
+        let either = envIO.provide(apiConfig).unsafeRunSyncEither()
+        let dataError = either.leftValue.dataError!
+        return (response: either, httpResponse: dataError.response, data: dataError.data)
+    }
+}
+
+private extension Either where A == API.HTTPError, B: Equatable {
+    func assert(success expected: B, information: String? = nil, file: StaticString = #file, line: UInt = #line) {
         fold({ error in XCTFail(information ?? "Expected successful value \(expected), but found an error: \(error).", file: file, line: line) },
              { value in XCTAssertEqual(expected, value, file: file, line: line) })
     }
 }
 
-extension Either where A == API.HTTPError {
-    func assert(error: API.HTTPError, _ information: String? = nil, file: StaticString = #file, line: UInt = #line) {
+private extension Either where A == API.HTTPError {
+    func assert(error: API.HTTPError, information: String? = nil, file: StaticString = #file, line: UInt = #line) {
         fold({ err in XCTAssertEqual(error, err, file: file, line: line) },
              { value in XCTFail(information ?? "Expected error: \(error), but found successful value: \(value)") })
     }
--- Tests/APIClientSendTests.swift
+++ Tests/APIClientSendTests.swift
@@ -4,6 +4,12 @@ import XCTest
 import Bow
 import BowEffects
 
+func send<T: Codable>(request: URLRequest) -> EnvIO<API.Config, API.HTTPError, T> {
+    EnvIO { apiConfig in
+        API.send(request: request, session: apiConfig.session, decoder: apiConfig.decoder)
+    }
+    
+}
 
 class APIClientSendTests: XCTestCase {
     
@@ -11,123 +17,122 @@ class APIClientSendTests: XCTestCase {
         let apiConfig = Mock.apiConfig.copy(decoder: StringUTF8Decoder())
                                       .stub(dataRaw: "data-success")
         
-        APITestCase(apiConfig: apiConfig)
-            .send(request: apiConfig.request)
-            .assert(success: "data-success")
+        assert(send(request: apiConfig.request),
+               withConfig: apiConfig,
+               succeeds: "data-success")
     }
 
-    func testAPIClient_ValidRequestAndInvalidData_ReturnError() {
-        let apiConfig = Mock.apiConfig.stub(error: Mock.Error.general)
-        
-        APITestCase(apiConfig: apiConfig)
-            .send(request: apiConfig.request)
-            .map(Either<API.HTTPError, String>.pure)^
-            .assert(error: .other(error: Mock.Error.general))
-    }
+//    func testAPIClient_ValidRequestAndInvalidData_ReturnError() {
+//        let apiConfig = Mock.apiConfig.stub(error: Mock.Error.general)
+//
+//        assert(send<String>(request: apiConfig.request),
+//               withConfig: apiConfig,
+//               failures: Mock.Error.general)
+//    }
 
-    func testAPIClient_ValidRequestAndInvalidDecoder_ReturnParsingError() {
-        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
-                                      .stub(dataRaw: "data-success")
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .parsingError(response: dataError.response, data: dataError.data))
-    }
+//    func testAPIClient_ValidRequestAndInvalidDecoder_ReturnParsingError() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
+//                                      .stub(dataRaw: "data-success")
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .parsingError(response: dataError.response, data: dataError.data))
+//    }
 
-    func testAPIClient_ValidRequestWithWrongDecoder_ReturnParsingError() {
-        let apiConfig = Mock.apiConfig.copy(decoder: JSONDecoder()) // expected: StringUTF8Decoder()
-                                      .stub(dataRaw: "data-success")
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .parsingError(response: dataError.response, data: dataError.data))
-    }
-
-    func testAPIClient_InvalidResponseWithCode400_ReturnBadRequest() {
-        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
-                                      .stub(dataRaw: "data-success", code: 400)
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .badRequest(response: dataError.response, data: dataError.data))
-    }
-
-    func testAPIClient_InvalidResponseWithCode403_ReturnForbiddenError() {
-        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
-                                      .stub(dataRaw: "data-success", code: 403)
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .forbidden(response: dataError.response, data: dataError.data))
-    }
-
-    func testAPIClient_InvalidResponseWithCode404_ReturnNotFoundError() {
-        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
-                                      .stub(dataRaw: "data-success", code: 404)
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .notFound(response: dataError.response, data: dataError.data))
-    }
-
-    func testAPIClient_InvalidResponseWithCode500_ReturnServerError() {
-        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
-                                      .stub(dataRaw: "data-success", code: 500)
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .serverError(response: dataError.response, data: dataError.data))
-    }
-
-    func testAPIClient_InvalidResponseWithCode503_ReturnServiceUnavailable() {
-        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
-                                      .stub(dataRaw: "data-success", code: 503)
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .serviceUnavailable(response: dataError.response, data: dataError.data))
-    }
-
-    func testAPIClient_InvalidResponseWithUnknownCode_ReturnUnknownError() {
-        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
-                                      .stub(dataRaw: "data-success", code: 69)
-        
-        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
-                                                            .send(request: apiConfig.request)
-        
-        let dataError = response.leftValue.dataError!
-        response.assert(error: .unknown(response: dataError.response, data: dataError.data))
-    }
-
-    func testAPIClient_EmptyJSONResponse_ReturnNoResponse() {
-        let apiConfig = Mock.apiConfig.copy(decoder: JSONDecoder())
-                                      .stub(dataRaw: "{}")
-        
-        APITestCase(apiConfig: apiConfig)
-            .send(request: apiConfig.request)
-            .assert(success: NoResponse())
-    }
-
-    func testAPIClient_EmptyStringResponse_ReturnNoResponse() {
-        let apiConfig = Mock.apiConfig.copy(decoder: StringUTF8Decoder())
-                                      .stub(dataRaw: "")
-        
-        APITestCase(apiConfig: apiConfig)
-            .send(request: apiConfig.request)
-            .assert(success: NoResponse())
-    }
+//    func testAPIClient_ValidRequestWithWrongDecoder_ReturnParsingError() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: JSONDecoder()) // expected: StringUTF8Decoder()
+//                                      .stub(dataRaw: "data-success")
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .parsingError(response: dataError.response, data: dataError.data))
+//    }
+//
+//    func testAPIClient_InvalidResponseWithCode400_ReturnBadRequest() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
+//                                      .stub(dataRaw: "data-success", code: 400)
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .badRequest(response: dataError.response, data: dataError.data))
+//    }
+//
+//    func testAPIClient_InvalidResponseWithCode403_ReturnForbiddenError() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
+//                                      .stub(dataRaw: "data-success", code: 403)
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .forbidden(response: dataError.response, data: dataError.data))
+//    }
+//
+//    func testAPIClient_InvalidResponseWithCode404_ReturnNotFoundError() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
+//                                      .stub(dataRaw: "data-success", code: 404)
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .notFound(response: dataError.response, data: dataError.data))
+//    }
+//
+//    func testAPIClient_InvalidResponseWithCode500_ReturnServerError() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
+//                                      .stub(dataRaw: "data-success", code: 500)
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .serverError(response: dataError.response, data: dataError.data))
+//    }
+//
+//    func testAPIClient_InvalidResponseWithCode503_ReturnServiceUnavailable() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
+//                                      .stub(dataRaw: "data-success", code: 503)
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .serviceUnavailable(response: dataError.response, data: dataError.data))
+//    }
+//
+//    func testAPIClient_InvalidResponseWithUnknownCode_ReturnUnknownError() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: Mock.Decoder.invalid)
+//                                      .stub(dataRaw: "data-success", code: 69)
+//
+//        let response: Either<API.HTTPError, String> = APITestCase(apiConfig: apiConfig)
+//                                                            .send(request: apiConfig.request)
+//
+//        let dataError = response.leftValue.dataError!
+//        response.assert(error: .unknown(response: dataError.response, data: dataError.data))
+//    }
+//
+//    func testAPIClient_EmptyJSONResponse_ReturnNoResponse() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: JSONDecoder())
+//                                      .stub(dataRaw: "{}")
+//
+//        APITestCase(apiConfig: apiConfig)
+//            .send(request: apiConfig.request)
+//            .assert(success: NoResponse())
+//    }
+//
+//    func testAPIClient_EmptyStringResponse_ReturnNoResponse() {
+//        let apiConfig = Mock.apiConfig.copy(decoder: StringUTF8Decoder())
+//                                      .stub(dataRaw: "")
+//
+//        APITestCase(apiConfig: apiConfig)
+//            .send(request: apiConfig.request)
+//            .assert(success: NoResponse())
+//    }
 }

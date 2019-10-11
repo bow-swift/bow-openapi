6322b6b45fb61b361e588b5395da83ead155b814
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2019-10-11 11:20:32 +0200

    update static-lib with commens in the PR

--- Bow OpenAPI/Generator/Support Files/Generator.h
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2019 The Bow Authors
-
-#import <Foundation/Foundation.h>
-
-@interface Generator : NSObject
-
-@end
--- Bow OpenAPI/Generator/Support Files/Generator.m
+++
@@ -1,7 +0,0 @@
-//  Copyright © 2019 The Bow Authors
-
-#import "Generator.h"
-
-@implementation Generator
-
-@end

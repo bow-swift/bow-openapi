270e50a30d2b47e6f4bb2eccbd34e7a2a6a49e07
Tomás <truizlop@gmail.com>
2019-11-18 12:43:00 +0100

    Use stubbed responses for specific endpoints

--- Templates/StubURL.swift
+++ Templates/StubURL.swift
@@ -12,64 +12,97 @@ struct StubbedResponse {
         self.content = .right(data)
     }
     
-    init(error: Error, statusCode: Int = 200) {
+    init(error: Error, statusCode: Int = 404) {
         self.statusCode = statusCode
         self.content = .left(error)
     }
 }
 
 class StubURL: URLProtocol {
-    private(set) static var data: Data?
-    private(set) static var error: Error?
-    private(set) static var statusCode: Int = 200
+    static let anyEndpoint = ">>>ANY-ENDPOINT<<<"
+    private static var stubs: [String: [StubbedResponse]] = [:]
     
-    /// Stubs methods
-    class func stub(data: Data, code: Int = 200) {
-        StubURL.data = data
-        StubURL.statusCode = code
+    private static func stub(response: StubbedResponse, for endpoint: String) {
+        if let responses = stubs[endpoint] {
+            stubs[endpoint] = responses + [response]
+        } else {
+            stubs[endpoint] = [response]
+        }
     }
     
-    class func stub(error: Error, code: Int = 400) {
-        StubURL.error = error
-        StubURL.statusCode = code
+    /// Stub methods
+    static func stub(data: Data, code: Int = 200, endpoint: String = anyEndpoint) {
+        stub(response: StubbedResponse(data: data, statusCode: code),
+             for: endpoint)
     }
     
-    class func stub(json: String, code: Int = 200) {
-        stub(data: json.data(using: .utf8)!, code: code)
+    static func stub(error: Error, code: Int = 404, endpoint: String = anyEndpoint) {
+        stub(response: StubbedResponse(error: error, statusCode: code),
+             for: endpoint)
     }
     
-    class func stub(contentsOfFile url: URL, code: Int = 200) {
+    static func stub(json: String, code: Int = 200, endpoint: String = anyEndpoint) {
+        stub(response: StubbedResponse(data: json.data(using: .utf8)!),
+             for: endpoint)
+    }
+    
+    static func stub(contentsOfFile url: URL, code: Int = 200, endpoint: String = anyEndpoint) {
         let content = try! String(contentsOf: url, encoding: .utf8)
-        stub(json: content, code: code)
+        stub(json: content, code: code, endpoint: endpoint)
     }
     
     /// Reset the stub to default values: data to nil, error to nil and status code to 200.
-    class func reset() {
-        StubURL.data = nil
-        StubURL.error = nil
-        StubURL.statusCode = 200
+    static func reset() {
+        stubs = [:]
+    }
+    
+    static func response(for request: URLRequest) -> StubbedResponse? {
+        if let url = request.url?.absoluteString {
+            for endpoint in stubs {
+                if url.hasSuffix(endpoint.key) {
+                    return consume(endpoint.key)
+                }
+            }
+        }
+        return consume(anyEndpoint)
+    }
+    
+    static func consume(_ endpoint: String) -> StubbedResponse? {
+        let queue = stubs[endpoint]
+        let response = queue?.first
+        stubs[endpoint] = Array(queue?.dropFirst() ?? [])
+        return response
     }
     
     // MARK: - URLProtocol methods
-    override class func canInit(with request:URLRequest) -> Bool {
+    override static func canInit(with request:URLRequest) -> Bool {
         return true
     }
     
-    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+    override static func canonicalRequest(for request: URLRequest) -> URLRequest {
         return request
     }
     
     override func startLoading() {
+        guard let stub = StubURL.response(for: request) else {
+            fatalError("No response stubbed for request: \(request)")
+        }
+        
         let header = request.allHTTPHeaderFields
-        let response = HTTPURLResponse(url: request.url!, statusCode: StubURL.statusCode, httpVersion: nil, headerFields: header)!
+        
+        let response = HTTPURLResponse(url: request.url!, statusCode: stub.statusCode, httpVersion: nil, headerFields: header)!
         client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
         
-        if let error = StubURL.error {
-            client?.urlProtocol(self, didFailWithError: error)
-        } else {
-            client?.urlProtocol(self, didLoad: StubURL.data ?? Data())
-            client?.urlProtocolDidFinishLoading(self)
-        }
+        stub.content.fold(
+            { error in
+                client?.urlProtocol(self, didFailWithError: error)
+                return
+            },
+            { data in
+                client?.urlProtocol(self, didLoad: data)
+                client?.urlProtocolDidFinishLoading(self)
+                return
+            })
     }
     
     override func stopLoading() { }

05704a8db52731bd3321174890cc265e1530b234
Tomás <truizlop@gmail.com>
2019-12-02 10:49:49 +0100

    Add content to Credits

--- Documentation.app/Contents/MacOS/Legal.playground/Pages/Credits.xcplaygroundpage/Contents.swift
+++ Documentation.app/Contents/MacOS/Legal.playground/Pages/Credits.xcplaygroundpage/Contents.swift
@@ -1,7 +1,20 @@
-//: [Previous](@previous)
-
-import Foundation
-
-var str = "Hello, playground"
-
-//: [Next](@next)
+// nef:begin:header
+/*
+ layout: docs
+ title: Credits
+ */
+// nef:end
+/*:
+ # Credits
+ 
+ 2019 - © The Bow Authors
+ 
+ For a comprehensive list of contributors, [visit the repository](https://github.com/bow-swift/bow-openapi/graphs/contributors) on GitHub.
+ 
+ Bow Open API is supported by [47 Degrees](https://www.47deg.com).
+ 
+ This library uses the following libraries or tools:
+ 
+ - [Bow](https://github.com/bow-swift/bow)
+ - [swagger-codegen](https://github.com/swagger-api/swagger-codegen)
+ */

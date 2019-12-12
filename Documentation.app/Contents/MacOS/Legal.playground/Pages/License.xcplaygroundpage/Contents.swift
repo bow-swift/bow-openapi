639145cce1cc573ff3f75c1faac9ec762e3ff71f
Tomás Ruiz-López <truizlop@gmail.com>
2019-12-12 17:17:01 +0100

    Update Documentation.app/Contents/MacOS/Legal.playground/Pages/License.xcplaygroundpage/Contents.swift
    Co-Authored-By: Miguel Ángel Díaz <madiaz.dev@gmail.com>

--- Documentation.app/Contents/MacOS/Legal.playground/Pages/License.xcplaygroundpage/Contents.swift
+++ Documentation.app/Contents/MacOS/Legal.playground/Pages/License.xcplaygroundpage/Contents.swift
@@ -7,7 +7,7 @@
 /*:
  # License
  
- Copyright 2018 The Bow OpenAPI Authors
+ Copyright 2019 The Bow OpenAPI Authors
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.

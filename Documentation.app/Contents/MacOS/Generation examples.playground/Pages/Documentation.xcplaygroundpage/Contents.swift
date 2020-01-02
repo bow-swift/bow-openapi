445048b44aa7fd651397005cad432c25813c89fc
Tomás <truizlop@gmail.com>
2020-01-02 16:01:20 +0100

    Content for Documentation

--- Documentation.app/Contents/MacOS/Generation examples.playground/Pages/Documentation.xcplaygroundpage/Contents.swift
+++ Documentation.app/Contents/MacOS/Generation examples.playground/Pages/Documentation.xcplaygroundpage/Contents.swift
@@ -7,4 +7,35 @@
 /*:
  # Documentation
  
+ Bow OpenAPI leverages the documentation added in the specification so that you have it available as inline docs in Xcode. For instance, consider the following example:
+ 
+ ```yaml
+ openapi: "3.0.0"
+ info:
+   title: My Store
+   version: "1.0.0"
+ paths:
+   /products:
+     get:
+       tags:
+         - Product
+       operationId: getProducts
+       description: Obtains all products sold by the store.
+       parameters:
+         - name: sortBy
+           in: query
+           required: true
+           description: Sorting criteria.
+           schema:
+             $ref: '#/components/schemas/SortBy'
+       responses:
+         '200':
+           description: All products in the store.
+           content:
+             application/json:
+               schema:
+                 $ref: '#/components/schemas/Products'
+ ```
+ 
+ The generator will take all the metadata in the fields named `description` and will use that information for documentation of the corresponding artifacts.
  */

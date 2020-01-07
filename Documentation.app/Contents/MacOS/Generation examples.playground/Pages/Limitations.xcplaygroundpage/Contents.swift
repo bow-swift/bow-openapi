c92661cebb60c189d042ba6c713675299dfd8c9c
Tomás <truizlop@gmail.com>
2020-01-07 17:49:01 +0100

    Clarify limitations

--- Documentation.app/Contents/MacOS/Generation examples.playground/Pages/Limitations.xcplaygroundpage/Contents.swift
+++ Documentation.app/Contents/MacOS/Generation examples.playground/Pages/Limitations.xcplaygroundpage/Contents.swift
@@ -13,9 +13,13 @@
  
  ## Inline data types
  
- In order to have a proper generation, you need to define data models in the `components` section in OpenAPI, or in the `definitions` section in Swagger. Defining your data models inline can result in generated code that is not properly named and therefore difficult to use.
+ In order to have a proper generation, you need to define data models in the `components` section in OpenAPI, or in the `definitions` section in Swagger. Defining your data models inline can result in generated code that is not properly named and therefore difficult to use. Nested data type definition is not supported either; you will need to extract those types to the root of the definition of your models.
  
  ## Error data
  
  If you are specifying data models as part of the error response of an endpoint, Bow OpenAPI will not parse that into a value object. However, you will be able to access such information as it is carried in the `API.HTTPError` value that you will get.
+ 
+ ## Body parameters encoding
+ 
+ Currently, the only supported encoding for body parameters is `application/json`.
  */

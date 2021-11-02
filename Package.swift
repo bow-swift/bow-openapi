17cc438b9739aa61d7646ca24ea70f4dc1dd7c3a
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2021-11-02 17:28:36 +0100

    Fix CI - adoptopenjdk8 (#46)

--- Package.swift
+++ Package.swift
@@ -6,35 +6,35 @@ extension Target.Dependency {
     static var bow: Target.Dependency {
         .product(name: "Bow", package: "Bow")
     }
-    
+
     static var bowEffects: Target.Dependency {
         .product(name: "BowEffects", package: "Bow")
     }
-    
+
     static var bowOptics: Target.Dependency {
         .product(name: "BowOptics", package: "Bow")
     }
-    
+
     static var swiftLine: Target.Dependency {
         .product(name: "Swiftline", package: "Swiftline")
     }
-    
+
     static var swiftCheck: Target.Dependency {
         .product(name: "SwiftCheck", package: "SwiftCheck")
     }
-    
+
     static var snapshotTesting: Target.Dependency {
         .product(name: "SnapshotTesting", package: "SnapshotTesting")
     }
-    
+
     static var argumentParser: Target.Dependency {
         .product(name: "ArgumentParser", package: "swift-argument-parser")
     }
-    
+
     static var fixturesAPI: Target.Dependency {
         .product(name: "FixturesAPI", package: "FixturesAPI")
     }
-    
+
     static var fixturesAPITest: Target.Dependency {
         .product(name: "FixturesAPITest", package: "FixturesAPI")
     }
@@ -54,7 +54,7 @@ extension Target {
             .cli
         ]
     }
-    
+
     static var openApiGenerator: Target {
         .target(name: "OpenApiGenerator",
                 dependencies: [.bow,
@@ -63,7 +63,7 @@ extension Target {
                                .swiftLine],
                 path: "OpenApiGenerator")
     }
-    
+
     static var cli: Target {
         .target(name: "CLI",
                 dependencies: [openApiGenerator.asDependency,
@@ -79,7 +79,7 @@ extension Target {
             .generatorTests
         ]
     }
-    
+
     static var generatorTests: Target {
         .testTarget(name: "OpenApiGeneratorTests",
                     dependencies: [Target.openApiGenerator.asDependency,
@@ -101,11 +101,11 @@ extension Target {
 // MARK: - Package
 let package = Package(
     name: "Bow OpenAPI",
-    
+
     platforms: [
         .macOS(.v10_14)
     ],
-    
+
     products: [
         .executable(name: "bow-openapi", targets: [Target.cli.name])
     ],
@@ -113,7 +113,7 @@ let package = Package(
     dependencies: [
         .package(name: "Bow", url: "https://github.com/bow-swift/bow.git", .exact("0.8.0")),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .exact("0.2.1")),
-        .package(url: "https://github.com/bow-swift/Swiftline.git", .exact("0.5.5")),
+        .package(url: "https://github.com/bow-swift/Swiftline.git", .exact("0.5.6")),
         .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .exact("1.7.2")),
         .package(url: "https://github.com/bow-swift/SwiftCheck.git", .exact("0.12.1")),
         .package(path: "./Tests/Fixtures/FixturesAPI"),
